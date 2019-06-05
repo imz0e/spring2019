75598c2527c7c5cb0b1a8ad689044ecdf055f435
z0its <z0story@naver.com>
2019-06-05 14:22:35 +0900

    init

---
+++ demo/src/main/java/com/example/demo/BookService.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+	
+	BookRepository bookRepository;
+	
+	@Autowired
+	public BookService(BookRepository bookRepository) {
+		this.bookRepository = bookRepository;
+	}
+
+}
